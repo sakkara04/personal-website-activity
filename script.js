b6d79abe68d29f77656477976649aa8be6dcc4fa
sharonakk <shar.akkara@gmail.com>
2024-10-18 00:12:03 -0700

    Added color-changing nav bar feature

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const colors = ["#afd69b", "#b7cc9d", "#a6dbbc", "#8fb6ab", "#8ca19e", "#b0c4b1"];
+
+function getColor() {
+    const i = Math.floor(Math.random() * colors.length);
+    return colors[i];
+}
+
+document.getElementById("navbar").addEventListener("click", function() {
+
+    navbar.style.backgroundColor = getColor();
+});
+
 document.getElementsByClassName("my-picture")[0].onclick = function(){
     alert("that's me!");
 }
@@ -9,4 +21,3 @@ document.getElementsByClassName("nav-button")[0].onclick = function(){
 document.getElementsByClassName("nav-button")[1].onclick = function(){
     alert("content about my projects coming soon...");
 }
-
