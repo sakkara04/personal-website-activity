fa80196b999f948e600d87e6ccd06ab1e147e795
sharonakk <shar.akkara@gmail.com>
2024-10-17 23:52:47 -0700

    Completed file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("my-picture")[0].onclick = function(){
+    alert("that's me!");
+}
+
+document.getElementsByClassName("nav-button")[0].onclick = function(){
+    alert("content about me coming soon...");
+}
+
+document.getElementsByClassName("nav-button")[1].onclick = function(){
+    alert("content about my projects coming soon...");
+}
+
